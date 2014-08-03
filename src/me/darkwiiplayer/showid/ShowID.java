21a87e4279a427ba8372a3528218c497fa90f2e0
DarkWiiPlayer <DarkWiiPlayer@hotmail.com>
2014-08-03 12:50:24 +0200

    Changed command handling
    Changed the way lognames and logjoins commands work internally, and
    improved the console output of these commands.

--- src/me/darkwiiplayer/showid/ShowID.java
+++ src/me/darkwiiplayer/showid/ShowID.java
@@ -23,10 +23,7 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 public class ShowID extends JavaPlugin implements Listener {
-	//OPTIONS:
-	private Boolean logNames = true;
-	private Boolean logJoins = true;
-	
+
 	public final Logger logger = Logger.getLogger("minecraft");
 	private static ShowID instance;
 	FileLoggerNames nameLogger = new FileLoggerNames();
@@ -34,38 +31,96 @@ public class ShowID extends JavaPlugin implements Listener {
 	public static Plugin getInstance() {
 		return instance;
 	}
-	
+
+	// === LOGGING OPTIONS ===
+
+	private Boolean logNames = true;
+
+	public boolean setNameLogging() {
+		logNames = !logNames;
+		if (logNames) {
+			logger.info("[SHOWID] Join Logging has been turned on");
+		} else {
+			logger.info("[SHOWID] Join Logging has been turned off");
+		}
+		return logNames;
+	}
+
+	public boolean setNameLogging(boolean x) {
+		logNames = x;
+		if (logNames) {
+			logger.info("[SHOWID] Join Logging has been turned on");
+		} else {
+			logger.info("[SHOWID] Join Logging has been turned off");
+		}
+		return logNames;
+	}
+
+	private Boolean logJoins = true;
+
+	public boolean setJoinLogging() {
+		logJoins = !logJoins;
+		if (logJoins) {
+			logger.info("[SHOWID] Join Logging has been turned on");
+		} else {
+			logger.info("[SHOWID] Join Logging has been turned off");
+		}
+		return logJoins;
+	}
+
+	public boolean setJoinLogging(boolean x) {
+		logJoins = x;
+		if (logJoins) {
+			logger.info("[SHOWID] Join Logging has been turned on");
+		} else {
+			logger.info("[SHOWID] Join Logging has been turned off");
+		}
+		return logJoins;
+	}
+
+	// === END LOGGING OPTIONS ===
+
 	@Override
-	public void onEnable() {		
-		//Loading Options:
+	public void onEnable() {
+		// Loading Options:
 		DumperOptions options = new DumperOptions();
 		options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
 		Yaml yaml = new Yaml(options);
-		
-		File file = new File("plugins/showid.yml");  //YAML file where every UUID and all of his usernames are stored
+
+		File file = new File("plugins/showid.yml"); // YAML file where every
+													// UUID and all of his
+													// usernames are stored
 		FileInputStream IStream;
-		Object input;		
-		
-		if (!file.exists()) { //Create the file if it doesn't exist, exit with FALSE if it doesn't work (file does not exist and cannot be created)
+		Object input;
+
+		if (!file.exists()) { // Create the file if it doesn't exist, exit with
+								// FALSE if it doesn't work (file does not exist
+								// and cannot be created)
 			try {
 				IStream = new FileInputStream(file);
-				
+
 				input = yaml.load(IStream);
-				
+
 				if (input instanceof HashMap) {
-					if (((HashMap)input).get("logNames").equals(false)) {logNames = false;}
-					if (((HashMap)input).get("logJoins").equals(false)) {logJoins = false;}
+					if (((HashMap) input).get("logNames").equals(false)) {
+						logNames = false;
+					}
+					if (((HashMap) input).get("logJoins").equals(false)) {
+						logJoins = false;
+					}
 				}
-				
+
 				IStream.close();
 			} catch (FileNotFoundException e) {
 				logger.info("No config file found, loading default values.");
-				//Not actually loading any dafault values, since the variables are already set to their defaults.
+				// Not actually loading any dafault values, since the variables
+				// are already set to their defaults.
 			} catch (IOException e) {
-				logger.info("Error loading config file! Java error: " + e.getMessage());
+				logger.info("Error loading config file! Java error: "
+						+ e.getMessage());
 			}
 		}
-		
+
 		getServer().getPluginManager().registerEvents(this, this);
 		instance = this;
 		logger.info("ShowID has been enabled.");
@@ -74,37 +129,43 @@ public class ShowID extends JavaPlugin implements Listener {
 	@Override
 	public void onDisable() {
 		String fileName;
-		File file;  //YAML file where every UUID and all of his usernames are stored
+		File file; // YAML file where every UUID and all of his usernames are
+					// stored
 		FileWriter FWriter;
-		
+
 		DumperOptions options = new DumperOptions();
 		Yaml yaml;
-		
+
 		logger.info("ShowID has been disabled.");
 	}
-	
-	
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 
 		if (cmd.getName().equalsIgnoreCase("showid")) {
-			if (sender instanceof Player) { // Command used by player ==========================================================
+			if (sender instanceof Player) { // Command used by player
 				if (args.length > 0) { // Request other player's ID
-					if (sender.hasPermission("uuid.others")) { // Check Permissions
+					if (sender.hasPermission("uuid.others")) { // Check
+																// Permissions
 						Player player = Bukkit.getServer().getPlayer(args[0]);
-						//Gets the player from the name used
+						// Gets the player from the name used
 
 						if (player != null) { // Valid Player
-							sender.sendMessage("[SHOWID] " + args[0] + "'s UUID is: "
-									+ player.getUniqueId());
-							if (player.hasPermission("uuid.tell.player") & !sender.hasPermission("uuid.others.silent")) {
-								player.sendMessage("[SHOWID] The player " + sender.getName() + " requested your UUID.\nYour UUID is: " + player.getUniqueId());
+							sender.sendMessage("[SHOWID] " + args[0]
+									+ "'s UUID is: " + player.getUniqueId());
+							if (player.hasPermission("uuid.tell.player")
+									& !sender
+											.hasPermission("uuid.others.silent")) {
+								player.sendMessage("[SHOWID] The player "
+										+ sender.getName()
+										+ " requested your UUID.\nYour UUID is: "
+										+ player.getUniqueId());
 							}
 						} else { // Not a valid player
-							sender.sendMessage("[SHOWID] Player \"" + args[0] + "\" not found!");
+							sender.sendMessage("[SHOWID] Player \"" + args[0]
+									+ "\" not found!");
 						}
 					} else {
 						sender.sendMessage("[SHOWID] You need the permission node uuid.others to do this!");
@@ -112,81 +173,154 @@ public class ShowID extends JavaPlugin implements Listener {
 
 				} else { // Request own ID
 					if (sender.hasPermission("uuid.self")) {
-						sender.sendMessage("[SHOWID] Your ID is: " + ((Player) sender).getUniqueId());
+						sender.sendMessage("[SHOWID] Your ID is: "
+								+ ((Player) sender).getUniqueId());
 					} else {
 						sender.sendMessage("[SHOWID] You need the permission node uuid.self to do this!");
 					}
 				}
-			return true;
-			} else { // Command used by console ================================================================================
+				return true;
+			} else { // Command used by console
 				if (args.length > 0) {
 					Player player = Bukkit.getServer().getPlayer(args[0]);
 					if (player != null) {
-						sender.sendMessage(args[0] + "'s UUID is: "	+ player.getUniqueId());
+						sender.sendMessage(args[0] + "'s UUID is: "
+								+ player.getUniqueId());
 						if (player.hasPermission("uuid.tell.console")) {
-							player.sendMessage("[SHOWID] The console requested your UUID.\nYour UUID is: " + player.getUniqueId());
+							player.sendMessage("[SHOWID] The console requested your UUID.\nYour UUID is: "
+									+ player.getUniqueId());
 						}
 					} else {
-						sender.sendMessage("[SHOWID] Player \"" + args[0] + "\" Not found! (type /list to get a list of all the players online)");
+						sender.sendMessage("[SHOWID] Player \""
+								+ args[0]
+								+ "\" Not found! (type /list to get a list of all the players online)");
 					}
 				} else {
 					sender.sendMessage("[SHOWID] You are a console. You have no UUID.");
 				}
-			}			
+			}
 			return true;
 		}
 		
+		//=== LOGNAMES ===
+
 		if (cmd.getName().equalsIgnoreCase("lognames")) {
-			if (sender instanceof Player) { //Check if called by player
-				Player player = (Player)sender;
-				if (player.hasPermission("uuid.options")) { //Has the required permission
+			if (sender instanceof Player) { // Check if called by player
+				Player player = (Player) sender;
+				if (player.hasPermission("uuid.options")) { // Has the required permission
 					if (args.length > 0) {
-						if (args[0].equalsIgnoreCase("on")) {logNames = true; logger.info("[SHOWID] Name Logging has been turned on"); return true;} else
-						if (args[0].equalsIgnoreCase("off")) {logNames = false; logger.info("[SHOWID] Name Logging has been turned off"); return true;}
-						else { logNames = !logNames;  return true;}//Invalid argument						
-					} else { logNames = !logNames;  return true;} //Called with no arguments
-				} else { //Doesn't have the required permission
+						if (args[0].equalsIgnoreCase("on")) {
+							setNameLogging(true);
+							sender.sendMessage("[SHOWID] Name logging has been turned on");
+							return true;
+						} 
+						if (args[0].equalsIgnoreCase("off")) {
+							setNameLogging(false);
+							sender.sendMessage("[SHOWID] Name logging has been turned off");
+							return true;
+						}
+						
+						// Invalid argument
+						sender.sendMessage("[SHOWID] invalid command syntax");
+						sender.sendMessage("[SHOWID] loggnames [on|off]");
+						return true;
+					} else { // Called with no arguments
+						if (setNameLogging()) {
+							sender.sendMessage("[SHOWID] Name logging has been turned on");
+						} else {
+							sender.sendMessage("[SHOWID] Name logging has been turned off");
+						}
+						return true;
+					}
+				} else { // Doesn't have the required permission
 					player.sendMessage("[SHOWID] You don't have the permission to do that!");
 				}
-			} else { //Called by console
-				if (args.length > 0) { //The actual command interpreting
-					if (args[0].equalsIgnoreCase("on")) {logNames = true; logger.info("[SHOWID] Name Logging has been turned on"); return true;} else
-					if (args[0].equalsIgnoreCase("off")) {logNames = false; logger.info("[SHOWID] Name Logging has been turned off"); return true;}
-					else { logNames = !logNames;  return true;} //Invalid argument
-				} else { logNames = !logNames;  return true;} //Called with no arguments
+			} else { // Called by console
+				if (args.length > 0) { // The actual command interpreting
+					if (args[0].equalsIgnoreCase("on")) {
+						setNameLogging(true);
+						return true;
+					}
+					if (args[0].equalsIgnoreCase("off")) {
+						setNameLogging(false);
+						return true;
+					}
+					// Invalid argument
+					logger.info("[SHOWID] lognames [on|off]");
+				} else { // Called with no arguments
+					setNameLogging();
+					return true;
+				}
 			}
 		}
+
+		//=== LOGJOINS ===
 		
 		if (cmd.getName().equalsIgnoreCase("logjoins")) {
-			if (sender instanceof Player) { //Check if called by player
-				Player player = (Player)sender;
-				if (player.hasPermission("uuid.options")) { //Has the required permission
+			if (sender instanceof Player) { // Check if called by player
+				Player player = (Player) sender;
+				if (player.hasPermission("uuid.options")) { // Has the required
+															// permission
 					if (args.length > 0) {
-						if (args[0].equalsIgnoreCase("on")) {logJoins = true; logger.info("[SHOWID] Join Logging has been turned on"); return true;} else
-						if (args[0].equalsIgnoreCase("off")) {logJoins = false; logger.info("[SHOWID] Join Logging has been turned off"); return true;}
-						else { logJoins = !logJoins;  return true;}//Invalid argument						
-					} else { logJoins = !logJoins;  return true;} //Called with no arguments
-				} else { //Doesn't have the required permission
-					player.sendMessage("[SHOWID] You don't have the permission to do that!");
+						if (args[0].equalsIgnoreCase("on")) {
+							setJoinLogging(true);
+							sender.sendMessage("[SHOWID] Join logging has been turned on");
+							return true;
+						}
+						if (args[0].equalsIgnoreCase("off")) {
+							setJoinLogging(false);
+							sender.sendMessage("[SHOWID] Join logging has been turned off");
+							return true;
+						} 
+						// Invalid argument
+						sender.sendMessage("[SHOWID] invalid command syntax");
+						sender.sendMessage("[SHOWID] loggjoins [on|off]");
+						return true;
+					} else {
+						// Called with no arguments
+						if (setJoinLogging()) {
+							sender.sendMessage("[SHOWID] Join logging has been turned on");
+						} else {
+							sender.sendMessage("[SHOWID] Join logging has been turned off");
+						}
+						return true;
+					}
+				} else {
+					// Doesn't have the required permission
+					player.sendMessage("[SHOWID] You don't have the permission to do that!"); return true;
 				}
-			} else { //Called by console
-				if (args.length > 0) { //The actual command interpreting
-					if (args[0].equalsIgnoreCase("on")) {logJoins = true; logger.info("[SHOWID] Join Logging has been turned on"); return true;} else
-					if (args[0].equalsIgnoreCase("off")) {logJoins = false; logger.info("[SHOWID] Join Logging has been turned off"); return true;}
-					else { logJoins = !logJoins;  return true;} //Invalid argument
-				} else { logJoins = !logJoins; return true;} //Called with no arguments
+				
+			} else { // Called by console
+				if (args.length > 0) { // The actual command interpreting
+					if (args[0].equalsIgnoreCase("on")) {
+						setJoinLogging(false);
+						return true;
+					}
+					if (args[0].equalsIgnoreCase("off")) {
+						setJoinLogging(false);
+						return true;
+					}
+					// Invalid argument
+					logger.info("[SHOWID] logjoins [on|off]");
+					
+				} else { // Called with no arguments
+					logJoins = !logJoins;
+					return true;
+				}
 			}
 		}
-		
-	return false;
+
+		return false;
 	}
 
-	//======== This is where the logging happenes: =======================================================================
-	
+	// ======== This is where the logging happenes:
+	// =======================================================================
+
 	@EventHandler
 	public void onLogin(PlayerLoginEvent event) {
 		if (logNames) {
-			nameLogger.logName(event.getPlayer().getUniqueId(), event.getPlayer().getName());			
+			nameLogger.logName(event.getPlayer().getUniqueId(), event
+					.getPlayer().getName());
 		}
 	}
 
